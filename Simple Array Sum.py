f61471c8c9171b96534abaa941684fd2af1831a6
Annaël Horthannice LALASON <140686269+Naelskrrrt@users.noreply.github.com>
2024-03-15 06:27:44 +0300

    Add files via upload

---
+++ Simple Array Sum.py
@@ -0,0 +1,3 @@
+n = int(input("Enter the number of elements: "))
+arr = list(map(int, input("Enter the elements (Separate with a space): ").split()))
+print("The sum of the elements is: ", sum(arr))
